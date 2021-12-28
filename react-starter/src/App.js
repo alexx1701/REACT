81312d4ff6fa9c6f381904ad0e54838bcce8a864
alexx1701 <92756383+alexx1701@users.noreply.github.com>
2021-12-28 18:46:36 -0500

    events

--- react-starter/src/App.js
+++ react-starter/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 //import components
 //import Tweet from './component/Tweet'
-import CreateTweet from './component/CreateTweet';
-import TweetList from './component/TweetList';
+//import CreateTweet from './component/CreateTweet';
+//import TweetList from './component/TweetList';
 
 function App() {
   //here is the place to write normal JS
   const name = 'alex';
   const message = "i am a message"
+
+  const sayHelloHandler = () => {
+    console.log('Hello');
+  }
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
-      <CreateTweet />
-      <TweetList name={name} message={message}/>
       
+      {/* <CreateTweet />
+      <TweetList name={name} message={message}/>*/}
+      <h1>Hello React</h1>
+      <button onClick={sayHelloHandler}>Click</button>
     </div>
   );
 }
